ee3f75fed8d41ce149405efacdaf601834483052
Frederico Sarmento <fredsarm.js@gmail.com>
2023-07-12 17:33:01 -0300

    Simplify the route /api/accEveEntries
    Remove the core body of the .get() method
    and replace with a module function that returns
    data selected from a database

--- backend/index.js
+++ backend/index.js
@@ -1,3 +1,4 @@
+const environVar  = require('dotenv').config();
 const express = require('express');
 const app = express();
 
@@ -11,3 +12,7 @@ app.use('/api', routes);
 app.listen(3000, () => {
   console.log('Server started on port: 3000');
 });
+
+if (environVar.error) {
+  throw environVar.error;
+}
