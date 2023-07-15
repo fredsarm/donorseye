d93a2ccdd8d6fb1d3d668e8712130f6bb7b7428a
Frederico Sarmento <fredsarm.js@gmail.com>
2023-07-15 14:38:17 -0300

    Change route, remove hardcoded columns from query

--- backend/routes/accounting/eve_entries.js
+++ backend/routes/accounting/eve_entries.js
@@ -1,21 +1,13 @@
 const express = require('express');
 const router = express.Router();
-const createSelectWhereAndOrder = require('../../sql_statements/selectWhereAndOrder');
+const queryFilteredAndOrdered = require('../../queries/queryFilteredAndOrdered');
 
+const schemaName = 'accounting';
+const tableName = 'eve_entries';
+const viewName = 'vw_eve_entries';
 
 router.get('/', async (req, res) => {
-  await createSelectWhereAndOrder(req, res, 'accounting.eve_entries', [
-    'entry_id',
-    'entry_date',
-    'occur_date',
-    'acc_id',
-    'entry_parent',
-    'entity_id',
-    'user_id',
-    'memo',
-    'debit',
-    'credit',
-    'balance']);
+  await queryFilteredAndOrdered(req, res,schemaName,tableName, viewName);
 });
 
 module.exports = router;
