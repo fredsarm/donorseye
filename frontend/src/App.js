85c3f7088d867ad1c6b807b127021c78df648fcd
Frederico Sarmento <fredsarm.js@gmail.com>
2023-07-15 14:42:55 -0300

    Impove App.js. Replace fetch with axios.
    Apply borders on the table.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,63 +1,77 @@
+// Importando React e os hooks 'useEffect' e 'useState' do pacote react
 import React, { useEffect, useState } from 'react';
-import axios from 'axios';
-import { useTable } from 'react-table';
+import './App.css';
 
+// Importando o axios
+import axios from 'axios';
+
+// Definição do componente funcional 'App'
 function App() {
+  // Definição do estado 'data' e da função 'setData' para alterar este estado. O estado inicial é um array vazio.
   const [data, setData] = useState([]);
+
+  // Definição do estado 'columns' e da função 'setColumns' para alterar este estado. O estado inicial também é um array vazio.
   const [columns, setColumns] = useState([]);
 
+  // Definição do hook 'useEffect'. Esse hook é executado uma vez após o primeiro render do componente, pois o segundo argumento é um array vazio.
   useEffect(() => {
-    axios.get('/api/accEveEntries?select[]=*&orderBy[0][column]=entry_id&orderBy[0][direction]=asc')
+    // Chamada da API '/api/accEveEntries' com o método axios.get. O parâmetro '?select[]=*' indica que queremos selecionar todos os campos disponíveis.
+    axios.get('/api/accEveEntries?select[]=*&where[0][column]=entry_id&where[0][operator]=%3C&where[0][value]=500')
+      // Manipulação dos dados recebidos.
       .then(response => {
+        // Definição do estado 'data' para os dados recebidos.
         setData(response.data);
+        // Se houver dados, a primeira posição do array é usada para obter as chaves do objeto, que são definidas como colunas.
         if (response.data.length > 0) {
-          setColumns(Object.keys(response.data[0]).map(key => {
-            return {
-              Header: key,
-              accessor: key
-            }
-          }));
+          setColumns(Object.keys(response.data[0]));
         }
       });
   }, []);
 
-  const {
-    getTableProps,
-    getTableBodyProps,
-    headerGroups,
-    rows,
-    prepareRow,
-  } = useTable({ columns, data })
-
+  // Renderização do componente
   return (
     <div className="App">
-      <table {...getTableProps()} style={{ border: 'solid 1px blue' }}>
+      <table>
         <thead>
-          {headerGroups.map(headerGroup => (
-            <tr {...headerGroup.getHeaderGroupProps()}>
-              {headerGroup.headers.map(column => (
-                <th {...column.getHeaderProps()} style={{ borderBottom: 'solid 3px red', background: 'aliceblue', color: 'black', fontWeight: 'bold', }}>
-                  {column.render('Header')}
-                </th>
-              ))}
-            </tr>
-          ))}
+          <h1>Cabeçalho da Tabela</h1>
         </thead>
-        <tbody {...getTableBodyProps()}>
-          {rows.map((row, i) => {
-            prepareRow(row);
-            return (
-              <tr {...row.getRowProps()}>
-                {row.cells.map(cell => {
-                  return <td {...cell.getCellProps()} style={{ padding: '10px', border: 'solid 1px gray' }}>{cell.render('Cell')}</td>
-                })}
-              </tr>
-            );
-          })}
+        <tbody>
+
+
+          <tr>
+            {/* Mapeamento de cada coluna para uma célula de cabeçalho (<th>) */}
+            {columns.map((column, index) => (
+              <th key={index}>
+                {column}
+              </th>
+            ))}
+          </tr>
+
+
+          {/* Mapeamento de cada objeto de dados para uma linha de tabela (<tr>) */}
+          {data.map((row, index) => (
+
+
+          <tr key={index}>
+            {/* Mapeamento de cada coluna para uma célula de tabela (<td>) */}
+            {columns.map((column, index) => (
+              <td key={index}>
+                {row[column]}
+              </td>
+            ))}
+          </tr>
+
+
+
+          ))}
         </tbody>
+        <tfoot>
+          <h1>Barra de Navegação</h1>
+        </tfoot>
       </table>
     </div>
   );
 }
 
+// Exportação do componente App como padrão
 export default App;
